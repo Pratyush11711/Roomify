bdfa8ae2b910e187a94c93cc665da564f826d887
Pratyush11711 <pratyush.epi.spn117@gmail.com>
2026-03-02 15:12:40 +0530

    Host and Upload Images

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,9 @@ import type { Route } from "./+types/home";
 import { ArrowRight, Clock, Layers } from "lucide-react";
 import Button from "components/ui/Button";
 import Upload from "components/Upload";
-import { useNavigate } from "react-router";
+import { renderMatches, useNavigate } from "react-router";
+import { useState } from "react";
+import { createProject } from "lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,9 +16,23 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   const navigate =useNavigate();
+  const [projects,setProjects] = useState<DesignItem[]>([])
   const handleUploadComplete = async(base64Image:string)=>{
     const newId = Date.now().toString();
-    navigate(`/visualizer/${newId}`);
+    const name = `Residence ${newId}`;
+    const newItem = {
+      id:newId, name, sourceImage:base64Image, renderedImage:undefined,
+      timestamp:Date.now()
+    }
+    const saved = await createProject({item:newItem,visibility:'private'})
+    setProjects((prev)=>[newItem,...prev])
+    navigate(`/visualizer/${newId}`,{
+      state:{
+        initialImage:saved?.sourceImage,
+        initialRendered: saved?.renderedImage || null,
+        name
+      }
+    });
     return true;
   }
   return (
@@ -71,10 +87,11 @@ export default function Home() {
             </div>
           </div>
           <div className="projects-grid">
+            {projects.map(({id,name,renderedImage,sourceImage,timestamp})=>
             <div className="project-card group">
               <div className="preview">
                 <img
-                  src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                  src={renderedImage || sourceImage}
                   alt="Project"
                 />
                 <div className="badge">
@@ -83,10 +100,10 @@ export default function Home() {
               </div>
               <div className="card-body">
                 <div>
-                  <h3>Project Manhattan</h3>
+                  <h3>{name}</h3>
                   <div className="meta">
                     <Clock size={12} />
-                    <span>{new Date("01.01.2026").toLocaleDateString()}</span>
+                    <span>{new Date(timestamp).toLocaleDateString()}</span>
                     <span>By Pratyush</span>
                   </div>
                 </div>
@@ -95,6 +112,8 @@ export default function Home() {
                 </div>
               </div>
             </div>
+            )}
+            
           </div>
         </div>
       </section>
--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,10 +1,21 @@
+import { useLocation } from "react-router"
 
 
 const visualizerId = () => {
-  return (
-    <div>
-      
-    </div>
+  const location = useLocation();
+  const {initialImage,name} = location.state || {}
+    return (
+    <section>
+      <h1>{name || 'Untitled Project'}</h1>
+      <div className="visualizer">
+        {initialImage && (
+          <div className="image-container">
+            <h2>Source Image</h2>
+            <img src={initialImage} alt="source" />
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
